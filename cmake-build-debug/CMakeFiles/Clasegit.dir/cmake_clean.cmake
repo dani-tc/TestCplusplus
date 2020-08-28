70d10ffe9a1ab7e01a560e6f97ebb316b349d679
Daniel Torres <danieltorres@MacBook-Pro-de-Daniel.local>
2020-08-28 18:22:32 -0500

    First commit

---
+++ cmake-build-debug/CMakeFiles/Clasegit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clasegit.dir/main.cpp.o"
+  "Clasegit"
+  "Clasegit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Clasegit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
